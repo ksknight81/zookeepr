7b0cbd32f50142fc546dfdcd8d3dc786c4cac963
kevin knight <ksknight81@gmail.com>
2022-07-13 21:45:23 -0400

    added fetch and linked pages

--- server.js
+++ server.js
@@ -101,6 +101,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+// wildcard - so that if its not defined it will receive the homepage as the response
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
